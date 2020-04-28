e09122432063622a1a60a57ba0390e30cd16507d
Philip Rodriguez <philiprodriguez@knights.ucf.edu>
2020-04-27 21:36:15 -0700

    Add first() and higher() methods and implementation for iterator() method ( which uses first() and higher() :) )

--- DiskBinaryTreeSet.java
+++ DiskBinaryTreeSet.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Set;
 import java.util.Stack;
@@ -466,6 +467,61 @@ public class DiskBinaryTreeSet<E extends Serializable & Comparable<E>> implement
         return anySucceed;
     }
 
+    private E higherRec(long currentAddress, E value) throws IOException, ClassNotFoundException {
+        if (currentAddress == -1) {
+            // No subtree here.
+            return null;
+        }
+        E currentValue = getPayloadObject(currentAddress);
+        if (currentValue.compareTo(value) <= 0) {
+            // We are smaller than or equal to value, so it could not be us, but it could be someone 
+            // in our right subtree. If our right subtree's got no answer, then we also have no answer.
+            return higherRec(getRightPointer(currentAddress), value); 
+        }
+        else {
+            // Current value is too big, it could be us, but it could also be in our left subtree.
+            E valLeft = higherRec(getLeftPointer(currentAddress), value);
+            if (valLeft == null) {
+                // We're the answer.
+                return currentValue;
+            } else {
+                // Better answer was found in our left subtree.
+                return valLeft;
+            }
+        }
+    }
+
+    public E higher(E value) {
+        try {
+            if (isEmpty()) {
+                return null;
+            }
+
+            return higherRec(getRootAddress(), value);
+        } catch (ClassNotFoundException exc) {
+            throw new IllegalStateException("Failed to parse node payload, cannot recover.");
+        } catch (IOException exc) {
+            throw new IllegalStateException("Failed to access tree on disk, cannot recover.");
+        }
+    }
+
+    public E first() {
+        try {
+            if (isEmpty()) {
+                throw new NoSuchElementException();
+            }
+            long currentAddress = getRootAddress();
+            while(getLeftPointer(currentAddress) != -1) {
+                currentAddress = getLeftPointer(currentAddress);
+            }
+            return getPayloadObject(currentAddress);
+        } catch (ClassNotFoundException exc) {
+            throw new IllegalStateException("Failed to parse node payload, cannot recover.");
+        } catch (IOException exc) {
+            throw new IllegalStateException("Failed to access tree on disk, cannot recover.");
+        }
+    }
+
     @Override
     public boolean remove(Object o) {
         throw new UnsupportedOperationException("Not implemented! Cannot remove from DiskBinaryTreeSet!");
@@ -473,7 +529,39 @@ public class DiskBinaryTreeSet<E extends Serializable & Comparable<E>> implement
 
     @Override
     public Iterator<E> iterator() {
-        throw new UnsupportedOperationException("Not yet implemented!");
+        return new Iterator<E>() {
+            private E current;
+
+            @Override
+            public synchronized boolean hasNext() {
+                if (current == null && actualSize() > 0)
+                    return true;
+                return higher(current) != null;
+            }
+
+            @Override
+            public synchronized E next() {
+                try {
+                    if (actualSize() <= 0)
+                        throw new NoSuchElementException();
+                    if (current == null) {
+                        current = first();
+                        return first();
+                    }
+
+                    current = higher(current);
+                    if (current == null)
+                        throw new NoSuchElementException();
+                    
+                    // Return a deep copy of current, not current itself, to avoid manipulation of the returned object breaking the iterator
+                    return getPayloadObject(findAddressForValue(current).addressStack.peek());
+                } catch (ClassNotFoundException exc) {
+                    throw new IllegalStateException("Failed to parse node payload, cannot recover.");
+                } catch (IOException exc) {
+                    throw new IllegalStateException("Failed to access tree on disk, cannot recover.");
+                }
+            }
+        };
     }
 
     @Override
@@ -512,13 +600,16 @@ public class DiskBinaryTreeSet<E extends Serializable & Comparable<E>> implement
         randomAccessFile.close();
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Testing DiskBinaryTreeSet against TreeSet!");
-        TreeSet<BigInteger> treeSet = new TreeSet<>();
+    /*
+        BELOW THIS LINE IS STATIC, TEST-ONLY CODE
+    */
 
+    private static void testContainsAndSize_RandomInsertions() throws Exception {
+        System.out.println("Running testContainsAndSize_RandomInsertions");
         Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
         DiskBinaryTreeSet<BigInteger> diskBinaryTreeSet = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTest.dbts"));
 
+        TreeSet<BigInteger> treeSet = new TreeSet<>();
         Random r = new Random();
         for (int i = 0; i < 100; i++) {
             int rand = r.nextInt();
@@ -536,20 +627,139 @@ public class DiskBinaryTreeSet<E extends Serializable & Comparable<E>> implement
                 }
             }
         }
+        System.out.println("PASS! :)");
+    }
 
-        Files.deleteIfExists(Paths.get("diskBinaryTreeSetTestInOrder.dbts"));
-        DiskBinaryTreeSet<BigInteger> diskBinaryTreeSetInOrder = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTestInOrder.dbts"));
+    private static void testContainsAndSize_InOrderInsertions() throws Exception {
+        System.out.println("Running testContainsAndSize_InOrderInsertions");
+        Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
+        DiskBinaryTreeSet<BigInteger> diskBinaryTreeSet = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTest.dbts"));
+
         for (int i = 0; i < 100; i++) {
             System.out.println("Inserting " + (i+1));
-            diskBinaryTreeSetInOrder.add(BigInteger.valueOf(i+1));
+            diskBinaryTreeSet.add(BigInteger.valueOf(i+1));
+
+            if (i+1 != diskBinaryTreeSet.size()) {
+                throw new IllegalStateException("Size does not match.\nExpected: " + (i+1) + "\nActual:   " + diskBinaryTreeSet.size());
+            }
 
             for (int j = 0; j <= i; j++) {
-                if (!diskBinaryTreeSetInOrder.contains(BigInteger.valueOf(j+1))) {
-                    throw new IllegalStateException("diskBinaryTreeSetInOrder missing " + (j+1));
+                if (!diskBinaryTreeSet.contains(BigInteger.valueOf(j+1))) {
+                    throw new IllegalStateException("diskBinaryTreeSet missing " + (j+1));
+                }
+            }
+        }
+        System.out.println("PASS! :)");
+    }
+
+    private static void testHigher_RandomInsertions() throws Exception {
+        System.out.println("Running testHigher_RandomInsertions");
+        Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
+        DiskBinaryTreeSet<BigInteger> diskBinaryTreeSet = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTest.dbts"));
+
+        TreeSet<BigInteger> treeSet = new TreeSet<>();
+        Random r = new Random();
+        for (int n = 0; n < 5; n++) {
+            for (int i = 0; i < 250; i++) {
+                int rand = r.nextInt();
+                System.out.println("Inserting " + rand);
+                treeSet.add(BigInteger.valueOf(rand));
+                diskBinaryTreeSet.add(BigInteger.valueOf(rand));
+            }
+
+            for (int i = 0; i < 1000; i++) {
+                int rand = r.nextInt();
+                System.out.print("Querying higher of " + rand + "...");
+                BigInteger tsv = treeSet.higher(BigInteger.valueOf(rand));
+                BigInteger dbtsv = diskBinaryTreeSet.higher(BigInteger.valueOf(rand));
+                System.out.println("TreeSet reported " + tsv + ", and DiskBinaryTreeSet reported " + dbtsv);
+                if (tsv == null && dbtsv == null) {
+                    continue;
+                } else if (tsv != null && tsv.compareTo(dbtsv) == 0) {
+                    continue;
+                } else {
+                    throw new IllegalStateException("Querying higher of " + rand + " failed! TreeSet reported " + tsv + ", but DiskBinaryTreeSet reported " + dbtsv);
                 }
             }
         }
 
         System.out.println("PASS! :)");
+    }
+
+    private static void testFirst_RandomInsertions() throws Exception {
+        System.out.println("Running testFirst_RandomInsertions");
+        for (int n = 0; n < 5; n++) {
+            Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
+            DiskBinaryTreeSet<BigInteger> diskBinaryTreeSet = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTest.dbts"));
+
+            TreeSet<BigInteger> treeSet = new TreeSet<>();
+            Random r = new Random();
+            
+            for (int i = 0; i < 250; i++) {
+                int rand = r.nextInt();
+                System.out.println("Inserting " + rand);
+                treeSet.add(BigInteger.valueOf(rand));
+                diskBinaryTreeSet.add(BigInteger.valueOf(rand));
+            }
+
+            System.out.print("Testing first...");
+            BigInteger tsv = treeSet.first();
+            BigInteger dbtsv = diskBinaryTreeSet.first();
+            System.out.println("TreeSet says " + tsv + " and DiskBinaryTreeSet says " + dbtsv);
+            if (!tsv.equals(dbtsv)) {
+                throw new IllegalStateException("First failed! TreeSet says " + tsv + " but DiskBinaryTreeSet says " + dbtsv);
+            }
+        }
+
+        System.out.println("PASS! :)");
+    }
+
+    private static void testIterator_RandomInsertions() throws Exception {
+        System.out.println("Running testIterator_RandomInsertions");
+        Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
+        DiskBinaryTreeSet<BigInteger> diskBinaryTreeSet = new DiskBinaryTreeSet<>(new File("diskBinaryTreeSetTest.dbts"));
+
+        TreeSet<BigInteger> treeSet = new TreeSet<>();
+        Random r = new Random();
+        for (int n = 0; n < 5; n++) {
+            for (int i = 0; i < 250; i++) {
+                int rand = r.nextInt();
+                System.out.println("Inserting " + rand);
+                treeSet.add(BigInteger.valueOf(rand));
+                diskBinaryTreeSet.add(BigInteger.valueOf(rand));
+            }
+
+            System.out.print("Iterating...");
+            Iterator<BigInteger> tsi = treeSet.iterator();
+            Iterator<BigInteger> dbtsi = diskBinaryTreeSet.iterator();
+            
+            while (tsi.hasNext()) {
+                if (!dbtsi.hasNext()) {
+                    throw new IllegalStateException("DiskBinaryTreeSet hasNext() was false when it should have been true when iterating!");
+                }
+                BigInteger tsv = tsi.next();
+                BigInteger dbtsv = dbtsi.next();
+                System.out.println("TreeSet thinks next is " + tsv + " and DiskBinaryTreeSet thinks next is " + dbtsv);
+                if (!tsv.equals(dbtsv)) {
+                    throw new IllegalStateException("Iterator next() failed! TreeSet reported " + tsv + ", but DiskBinaryTreeSet reported " + dbtsv);
+                }
+            }
+            if (dbtsi.hasNext()) {
+                throw new IllegalStateException("DiskBinaryTreeSet hasNext() was true when it should have been false when iterating!");
+            }
+        }
+
+        System.out.println("PASS! :)");
+    }
+
+    public static void main(String[] args) throws Exception {
+        testContainsAndSize_RandomInsertions();
+        testContainsAndSize_InOrderInsertions();
+        testHigher_RandomInsertions();
+        testFirst_RandomInsertions();
+        testIterator_RandomInsertions();
+
+        Files.deleteIfExists(Paths.get("diskBinaryTreeSetTest.dbts"));
+        System.out.println("PASS ALL! :)");
     }
 }
